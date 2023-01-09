4d5d732a0b826520942ae9a4cadbbc306c6db1e6
Trevor <trevormngwenya@gmail.com>
2023-01-09 18:13:43 +0200

    Prep2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        string letter = "";
+
+        if (int.Parse(gradePercentage) >= 90)
+        {
+            letter = "A";
+        }
+        else if (int.Parse(gradePercentage) >= 80)
+        {
+            letter = "B";
+        }
+        else if (int.Parse(gradePercentage) >= 70)
+        {
+            letter = "C";
+        }
+        else if (int.Parse(gradePercentage) >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade percentage is {gradePercentage} - Grade: {letter}");
+
+        if (int.Parse(gradePercentage) >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass but don't give up. Give it another try.");
+        }
     }
 }

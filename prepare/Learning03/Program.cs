380c7eb4793a81e5b527611d5f67c23a460ca141
Trevor <ngw21001@byui.edu>
2023-01-19 12:23:13 +0200

    Learning 03 Faraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
+
+        f1.SetTop(3);
+        f1.SetBottom(5);
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine($"{f1.GetDecimalValue()}");
     }
 }

5e41611b73d00e0ae0168feb8b5d0532f4d6bd1b
Trevor <ngw21001@byui.edu>
2023-01-20 18:57:11 +0200

    Develop03 begin

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,20 @@
+public class Scripture
+{
+    private string _reference;
+    private string _scripture;
+
+    public string MakeScripture()
+    {
+        return "";
+    }
+
+    public string HideWords()
+    {
+        return "";
+    }
+
+    public string CompletelyHidden()
+    {
+        return "";
+    }
+}

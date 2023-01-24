59592f6fbf9a8d999aa330b17012d4b1a4c3b349
Trevor <ngw21001@byui.edu>
2023-01-24 18:32:17 +0200

    Learning 04 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Trevor Ngwenya", "Abstraction");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Felicity Moyo", "Fractions", "Section 7.3", "Problems 9-12");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Talent Ndlovu", "Ancient Music", "Origins of the Keyboard");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }

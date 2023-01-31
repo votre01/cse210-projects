c5c7bc868a86f0f44b3a32bda0a3db9cf960112b
Trevor <ngw21001@byui.edu>
2023-01-31 12:11:28 +0200

    Learning 05 complete

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,24 @@
+public class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0.0d;
+    }
+}

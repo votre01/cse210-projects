cc13ff9fc8f9f3d1db14bc84a29854490ad66d81
Trevor <ngw21001@byui.edu>
2023-02-14 10:18:41 +0200

    Final project draft

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        // Test vehicle class
+        Vehicle vehicle = new Vehicle();
+        vehicle.SetVehicleName();
+        vehicle.SetVehicleModel();
+        vehicle.SetRegistrationNumber();
+        vehicle.SetVehicleCategory();
+
+        Console.WriteLine($"{vehicle.GetVehicleName()}\n{vehicle.GetVehicleModel()}\n{vehicle.GetRegistrationNumber()}\n{vehicle.GetVehicleCategory()}");
     }
 }
--- final/FinalProject/Vehicle.cs
+++ final/FinalProject/Vehicle.cs
@@ -1,4 +1,69 @@
 public class Vehicle
 {
+    private string _vehicleName;
+    private string _vehicleModel;
+    private string _registrationNumber;
+    private string _vehicleCategory;
+    private bool _availability;
+
+    public Vehicle()
+    {
+
+    }
+
+    public virtual double Billing(double rate, int duration)
+    {
+        double bill = (double)(rate * duration);
+
+        return bill;
+    }
     
+    public bool Available()
+    {
+        return _availability;
+    }
+
+    public void SetVehicleName()
+    {
+        Console.Write("Enter vehicle name: ");
+        _vehicleName = Console.ReadLine();
+    }
+
+    public void SetVehicleModel()
+    {
+        Console.Write("What is the vehicle model: ");
+        _vehicleModel = Console.ReadLine();
+    }
+
+    public void SetRegistrationNumber()
+    {
+        Console.Write("Enter vehicle registration number: ");
+        _registrationNumber = Console.ReadLine();
+    }
+
+    public void SetVehicleCategory()
+    {
+        Console.Write("What category does the vehicle belong to: ");
+        _vehicleCategory = Console.ReadLine();
+    }
+
+    public string GetVehicleName()
+    {
+        return _vehicleName;
+    }
+
+    public string GetVehicleModel()
+    {
+        return _vehicleModel;
+    }
+
+    public string GetRegistrationNumber()
+    {
+        return _registrationNumber;
+    }
+
+    public string GetVehicleCategory()
+    {
+        return _vehicleCategory;
+    }
 }
